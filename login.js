285edc8f35aec92f675e4f75acd00043ac974bd6
Prabhat Choudhary <91115520+PrabhatZWS@users.noreply.github.com>
2021-11-12 10:55:18 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,44 @@
+var UserData = [],DataHolder=[],flaglogin=0;
+function ChekLoger(){
+    if('flag' in localStorage){
+        flaglogin=JSON.parse(localStorage.getItem('flag'));
+    }
+    if(flaglogin==0){
+       if(document.getElementById('UserName').value.length != 0){
+        FetchUser();
+       }else{
+           alert('Do not Submit Blank Username');
+       }
+    }else{
+        alert('A user Alredy Log IN');
+    }
+}
+function FetchUser() {
+    if ('UserData' in localStorage) {
+        UserData = JSON.parse(localStorage.getItem('UserData'));
+        for (let i = 0; i < UserData.length; i++) {
+            if (UserData[i].UserName == document.getElementById('UserName').value) {
+                if (UserData[i].Password == document.getElementById('Password').value) {
+                    document.getElementById('msg').innerHTML = ' ';
+                    document.getElementById('msg').innerHTML = 'Welcome to InstaGram';
+                    DataHolder.push(UserData[i]);
+                    localStorage.setItem('UserAccountData',JSON.stringify(DataHolder));
+                    flaglogin++;
+                    localStorage.setItem('flag',JSON.stringify(flaglogin));
+                    window.location.href='/User.html';
+                    break;
+                } else {
+                    document.getElementById('msg').innerHTML = ' ';
+                    document.getElementById('msg').innerHTML = ' Password NOt Match With UserName '
+                    break;
+                }
+            } else {
+                document.getElementById('msg').innerHTML = ' ';
+                document.getElementById('msg').innerHTML = ' User not Found '
+            }
+        }
+    } else {
+        alert('first Create Account');
+    }
+
+}
